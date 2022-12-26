b4ba5cb3a200b81d1735820195b9bbe9d581f64f
bazoy <bazoy@yandex.ru>
2022-12-26 20:56:03 +0300

    add constants

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 # Это файл конфигурации приложения, здесь может хранится путь к бд, ключ шифрования, что-то еще.
 # Чтобы добавить новую настройку, допишите ее в класс.
+from constants import ADD_DB
+
 
 # Пример
 
 class Config(object):
     DEBUG = True
     SECRET_HERE = '249y823r9v8238r9u'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
+    SQLALCHEMY_DATABASE_URI = ADD_DB
     SQLALCHEMY_TRACK_MODIFICATIONS = False
